0e64e2090bb58c9a246752d49716b748400519e2
unknown <objectifmh@hotmail.com>
2020-12-30 15:24:24 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "main", component: AppComponent},
+  {path: '**', redirectTo: '/main' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true,
+    onSameUrlNavigation: 'reload',
+    relativeLinkResolution: 'legacy',
+    scrollPositionRestoration: 'enabled',
+    anchorScrolling: 'enabled',
+  })],
+exports: [RouterModule]
 })
+
 export class AppRoutingModule { }

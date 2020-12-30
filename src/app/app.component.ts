80457266c1f8119fd7adf7452a0dba6649a0219c
unknown <objectifmh@hotmail.com>
2020-12-30 13:46:30 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'anais';
+  isShowMenu = true; 
+
+
+
+  ngOnInit() {
+    AOS.init();
+    console.log("je suis dans init")
+  }
+
+  getShowMenu() {
+    this.isShowMenu = !this.isShowMenu;
+    console.log("je suis dans show menu");
+
+  }
 }
+
+
+
